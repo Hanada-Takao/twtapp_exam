2c33ab719f8eb73aa48b566a5acd55d5c03bfbe3
Hanada-Takao <taaa0823@gmail.com>
2021-12-02 17:25:12 +0900

    commonalize_mounting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -13,11 +16,28 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
   def show
-    @post = Post.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    def update
+      if @post.update(post_params)
+        redirect_to posts_path, notice: "Postを編集しました！"
+      else
+        render :edit
+      end
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
