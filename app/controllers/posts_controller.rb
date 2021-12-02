3e246f86e398b2edaa1d4239c29e30a9f495a8dd
Hanada-Takao <taaa0823@gmail.com>
2021-12-02 18:25:24 +0900

    Make_the_post_confirmation_screen_appear_only_when_there_is_a_new_action_mounting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "Postを投稿しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "Postを投稿しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -40,6 +44,7 @@ class PostsController < ApplicationController
 
   def confirm
     @post = Post.new(post_params)
+    render :new if @post.invalid?
   end
 
   private
